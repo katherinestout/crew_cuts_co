90a31ade91bc5ce8d4d4febc61845dbc5e96af25
katherinestout <kjs14@hood.edu>
2019-06-12 14:50:18 -0400

    navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './../style/navbar.css';
 
-export default class Navbar extends Component {
+class Navbar extends Component {
+
+
+//for collapsable nav
+myFunction(){
+    const x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+
     render() {
         return (
-            <div>
+            <div className="navbar">
+                <div className="topnav"
+                id = "myTopnav">
+
+                    <div className="nav-container">
+                        <a>Contact</a>
+                        <a>Reviews</a>
+            <a>
+
+            <i class="fas fa-bars"></i>
+            </a>
+   
+
+
+                    </div>
+
+                </div>
                 
             </div>
         )
     }
 }
+
+export default Navbar;
